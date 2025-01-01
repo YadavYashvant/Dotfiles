fbee4a603aa65c42000e067686137b3611c41fcd
YadavYashvant <yashvantyadav855@gmail.com>
2025-01-01 17:42:19 +0530

    Auto-update: 2025-01-01 17:42:19

--- .config/ags/scripts/color_generation/applycolor.sh
+++ .config/ags/scripts/color_generation/applycolor.sh
@@ -98,7 +98,7 @@ apply_hyprland() {
     cp "scripts/templates/hypr/hyprland/colors.conf" "$CACHE_DIR"/user/generated/hypr/hyprland/colors.conf
 
     # Define colors
-    active_color=""
+    active_color="#C08B61"
     inactive_border="#00ff00"
 
     # Apply colors
